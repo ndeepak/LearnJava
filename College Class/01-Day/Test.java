3d2447b1e55d8c0469b55e952ce5c737f4f7b346
Deepak Nagarkoti <nagarkotideepak9843@gmail.com>
2022-11-16 10:35:57 +0545

    Test only

---
+++ College Class/01-Day/Test.java
@@ -0,0 +1,10 @@
+class Test
+{
+	public static void main(String[] args)
+	{
+		int a=20;
+		System.out.println("Hello WOrld" + a);
+		System.out.println("%d","\n",a);
+		System.out.println("Hello World!");
+	}
+}
