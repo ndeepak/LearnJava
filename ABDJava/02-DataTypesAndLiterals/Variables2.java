e89558282cbcbc6adbbfc8f8ebf61f1a791d63e3
Deepak Nagarkoti <nagarkotideepak9843@gmail.com>
2022-11-17 18:32:21 +0545

    Variables Name

---
+++ ABDJava/02-DataTypesAndLiterals/Variables2.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+
+class Variables2
+{
+    public static void main(String args[])
+    {
+        int x; // Allowed
+        int X; //Allowed
+        int roll number; // not allowed
+
+        int roll_number$student; //allowed
+        int 1x; // Not allowed
+
+        int _x; // Allowed
+
+        float int; // Not Allowed as Keyword
+
+        int string=10;
+        System.out.println(strings);
+        
+        int rollNumberOfStudent;
+        float averageMarksOfClass;
+
+    }
+}
