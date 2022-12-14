2b5bf408661c365f757e171d9f05677e3c46e759
Deepak Nagarkoti <nagarkotideepak9843@gmail.com>
2022-12-14 11:24:17 +0545

    Stop in java

---
+++ College Class/EclispseJavaWorkSpace/Unit1ProgrammingJava/src/concurrencyPackage/Stop.java
@@ -0,0 +1,36 @@
+package concurrencyPackage;
+
+public class Stop extends Thread
+{
+	@Override
+	public void run()
+	{
+		for(int i=0; i<5; i++)
+		{
+			try
+			{
+				sleep(500);
+				System.out.println(Thread.currentThread().getName());
+			}
+			catch(InterruptedException e)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+	public static void main(String[] args) 
+	{
+		// Creating three threads
+		Stop t1 = new Stop();
+		Stop t2 = new Stop();
+		Stop t3 = new Stop();
+		
+		// Call run() method
+		t1.start();
+		t2.start();
+		t3.stop();
+		System.out.println("Thread t3 is stopped.");
+	}
+
+}
