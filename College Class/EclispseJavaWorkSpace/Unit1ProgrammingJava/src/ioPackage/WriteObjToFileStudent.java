564ae7e2c20488704822d9438045f2bc19a0c60c
Deepak Nagarkoti <nagarkotideepak9843@gmail.com>
2022-12-21 13:19:43 +0545

    Write Object to file Student

---
+++ College Class/EclispseJavaWorkSpace/Unit1ProgrammingJava/src/ioPackage/WriteObjToFileStudent.java
@@ -0,0 +1,40 @@
+package ioPackage;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class WriteObjToFileStudent 
+{
+
+	public static void main(String[] args) throws IOException 
+	{
+		FileOutputStream fo = null;
+		ObjectOutputStream oo = null;
+		Student s1 = new Student(1, "Deep", 21);
+		Student s2 = new Student(2, "Jeep", 31);
+		File file = new File("E:\\01 Learn Java\\LearnJava\\College Class\\EclispseJavaWorkSpace\\Unit1ProgrammingJava\\src\\ioPackage\\JavaFiles\\objectfile.txt");
+		
+		
+		try
+		{
+			fo = new FileOutputStream(file);
+			oo = new ObjectOutputStream(fo);
+			
+			oo.writeObject(s1);
+			oo.writeObject(s2);
+			
+		}
+		catch(IOException e)
+		{
+			fo.close();
+			oo.close();
+		}
+//		finally
+//		{
+//			
+//		}
+	}
+
+}
